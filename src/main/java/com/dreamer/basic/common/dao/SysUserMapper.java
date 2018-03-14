18d9967f1fe6f58cfad1d1418d7145c215395527
sunjinfa <sunjinfa@jbinfo.cn>
2018-03-14 17:36:27 +0800

    提交mybatis-generator自动生成代码

---
+++ src/main/java/com/dreamer/basic/common/dao/SysUserMapper.java
@@ -0,0 +1,17 @@
+package com.dreamer.basic.common.dao;
+
+import com.dreamer.basic.common.entity.SysUser;
+
+public interface SysUserMapper {
+    int deleteByPrimaryKey(String userId);
+
+    int insert(SysUser record);
+
+    int insertSelective(SysUser record);
+
+    SysUser selectByPrimaryKey(String userId);
+
+    int updateByPrimaryKeySelective(SysUser record);
+
+    int updateByPrimaryKey(SysUser record);
+}
